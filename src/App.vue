4c0c7c8868c3b81bf64963f54058b13c2cba2e16
anastasiya-charnahayeva <anastasiya.charnahayeva@innowise.com>
2024-06-18 12:22:52 +0200

    header and sign out

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
 
 <template>
   <div>
+    <Header v-if="user"></Header>
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
+  import { storeToRefs } from "pinia";
   import { onMounted } from "vue";
-
   import { useAuthStore } from "./stores/modules/useAuth";
-
   import { onAuthStateChanged } from "firebase/auth";
   import fbAuth from "./firebase/index";
+  import Header from "./components/Header.vue";
 
   const authStore = useAuthStore();
-
   const { setCurrentUser } = authStore;
+  const { user } = storeToRefs(authStore);
 
   onMounted(() => {
     onAuthStateChanged(fbAuth, (user) => {
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="bg-gray-900">
+    <nav class="container mx-auto p-4 flex justify-end text-white">
+      <div class="flex-1">
+        <router-link to="/home" class="hover:text-blue-500"> Home </router-link>
+      </div>
+      <div class="flex-1">
+        <router-link to="/paint" class="hover:text-blue-500">
+          Paint
+        </router-link>
+      </div>
+      <button class="hover:text-blue-500" @click="signOut">Sign Out</button>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+  import { useAuthStore } from "../stores/modules/useAuth";
+  import { useRouter } from "vue-router";
+  import { ref } from "vue";
+  const { signOutUser } = useAuthStore();
+  const router = useRouter();
+
+  async function signOut() {
+    router.push({ path: "/login" });
+    await signOutUser();
+  }
+</script>
